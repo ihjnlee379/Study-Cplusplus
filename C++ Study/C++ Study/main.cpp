1fca980a44aff9272b6c2c845c2a3f6badfd4113
ihjnlee379 <ihjnlee379@gist.ac.kr>
2020-11-19 13:00:37 +0900

    initialer_list study

---
+++ C++ Study/C++ Study/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <initializer_list>
+
+int main() {
+	using namespace std;
+	initializer_list<double> test = { 1, 2.2, 33, 1 };
+	cout << test.size() << endl;
+	cout << endl;
+	double sum{};
+	cout << sum << endl;
+	for (auto p : test) {
+		sum += p;
+	}
+	cout << sum << endl;
+	sum = 0.0;
+	for (auto p = test.begin(); p != test.end(); ++p) {
+		sum += *p;
+	}
+	cout << sum << endl;
+	return 0;
+}
